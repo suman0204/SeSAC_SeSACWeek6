c17f84335e2a772e4ab5d6ba703e9a52cc3887ab
sumanhong <suman0204@naver.com>
2023-08-22 00:12:44 +0900

    Day 25 수업 - CodeBase, NSLayoutConstraints 사용해서 Autolayout 코드 베이스로 대응하기

--- SeSACWeek6/ViewController.swift
+++ SeSACWeek6/ViewController.swift
@@ -7,11 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//StoryBoard 방식
+//1. 객체 얹고, 레이아웃 잡고, 아웃렛 연결, 속성 조절
 
+//CodeBase 방식
+//1. 뷰객체 프로퍼티 선언 (클래스 인스턴스 생성)
+//2. 명시적으로 루트뷰에 추가 ( + translatesAutoresizingMaskIntoConstraints)
+//3. 크기와 위치 정의
+//4. 속성 정의
+// => Frame 한계
+// => AutoResizingMask, AutoLayout => 지금까지는 스토리보드로 대응했다
+// => NSLayoutConstraints를 사용해서 오토레이웃을 코드 베이스로 대응
+
+class ViewController: UIViewController {
+    
+    //뷰 객체 프로퍼티 선언
+    let emailTextField = UITextField()
+    let passwordTextField = UITextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 루트뷰에 추가
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        emailTextField.frame = CGRect(x: 50, y: 80, width: UIScreen.main.bounds.width - 100, height: 50)
+        
+        emailTextField.backgroundColor = .lightGray
+        emailTextField.isSecureTextEntry = true
+        emailTextField.keyboardType = .numberPad
+        emailTextField.placeholder = "닉네임을 입력해주세요"
+        
+        NSLayoutConstraint(item: passwordTextField, attribute: .leading, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .leading, multiplier: 1, constant: 50).isActive = true
+            //레이아웃 활성화
+        
+        NSLayoutConstraint(item: passwordTextField, attribute: .trailing, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .trailing, multiplier: 1, constant: -50).isActive = true
+        
+        NSLayoutConstraint(item: passwordTextField, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 60).isActive = true
+        
+        NSLayoutConstraint(item: passwordTextField, attribute: .top, relatedBy: .equal, toItem: emailTextField, attribute: .bottom, multiplier: 1, constant: 50).isActive = true
+        
+        passwordTextField.backgroundColor = .brown
     }
 
 
